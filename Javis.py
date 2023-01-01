ba0eed22596a5a3c44fd3a350874b92942d93a13
TK17250 <94910246+TK17250@users.noreply.github.com>
2023-01-02 00:05:59 +0700

    edit Javis.py

--- Javis.py
+++ Javis.py
@@ -3,7 +3,7 @@ import pyttsx3 as tx
 engine = tx.init()
 
 TH_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_THAI"
-TH = engine.setProperty("voice", TH_voice_id)
+engine.setProperty("voice", TH_voice_id)
 
 engine.say("ผมคือ หุ่นยนต์ พูดภาษาไทยได้ครับ")
 engine.runAndWait()
