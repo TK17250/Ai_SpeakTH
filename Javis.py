ed5aef2daaf3a6c8387b3dff50730caebc41e810
TK17250 <94910246+TK17250@users.noreply.github.com>
2023-01-01 23:45:17 +0700

    first commit

---
+++ Javis.py
@@ -0,0 +1,9 @@
+import pyttsx3 as tx
+
+engine = tx.init()
+
+TH_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_THAI"
+TH = engine.setProperty("voice", TH_voice_id)
+
+engine.say("ผมคือ หุ่นยนต์ พูดภาษาไทยได้ครับ")
+engine.runAndWait()
