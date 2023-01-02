baf22aff22c8fc54fc266029d52ad4db11a34531
TK17250 <94910246+TK17250@users.noreply.github.com>
2023-01-02 09:50:54 +0700

    edit Jvis.py

--- Javis.py
+++ Javis.py
@@ -2,8 +2,8 @@ import pyttsx3 as tx
 
 engine = tx.init()
 
-TH_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_THAI"
-engine.setProperty("voice", TH_voice_id)
+TH_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_THAI" # เพิ่มเสียงภาษาไทย
+engine.setProperty("voice", TH_voice_id) # ตั้งให้เป็นเสียงภาษาไทย
 
 engine.say("ผมคือ หุ่นยนต์ พูดภาษาไทยได้ครับ")
-engine.runAndWait()
+engine.runAndWait() # รอให้พูดจบ
